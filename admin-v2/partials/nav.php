5c7d383b8060686919f36f188558b9bed37b1262
Adrien Rousselet <nibblerz@gmail.com>
2015-10-27 14:52:06 +0100

    Add admin-v2

---
+++ admin-v2/partials/nav.php
@@ -0,0 +1,9 @@
+<ul class="sidebar-menu">
+	<li class="header">MAIN NAVIGATION</li>
+	<li>
+	  <a href="pages/mailbox/mailbox.html">
+	    <i class="fa fa-envelope"></i> <span>Mailbox</span>
+	    <small class="label pull-right bg-yellow">12</small>
+	  </a>
+	</li>
+</ul>
